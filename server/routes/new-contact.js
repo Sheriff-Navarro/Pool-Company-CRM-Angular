05c4ff144e1d1ba401c1bd0c6b6d31e499db1a63
Aaron Molina <aaronm-git@users.noreply.github.com>
2017-12-17 16:58:54 -0500

    removed new-contact route code which was crashing app. I'll be working on angular and the first page of the app.

--- server/routes/new-contact.js
+++ server/routes/new-contact.js
@@ -4,43 +4,6 @@ const { ensureLoggedIn } = require('connect-ensure-login');
 const mongoose = require('mongoose');
 
 const router = express.Router();
-//--------------------------------------------------------Sign up route
-router.post('/:id', (req, res, next) => {
-  //second step of sign up
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const companyName = req.body.companyName;
-  const phonePrimary = req.body.phonePrimary;
-  const street = req.body.street;
-  const city = req.body.city;
-  const province = req.body.province;
 
-    const theClient = new User({
-      firstName : clientInfo.firstName,
-      lastName : clientInfo.lastName,
-      companyName : clientInfo.companyName,
-      phonePrimary : clientInfo.phonePrimary,
-      street : clientInfo.street,
-      city : clientInfo.city,
-      province : clientInfo.province,
-    });
-
-    theClient.save((err) => {
-      if (err){
-        res.status(500).json({message: 'Something went wrong creating your account in the database. User account was not created.'});
-        return;
-      }
-
-      req.login(theUser, (err) => {
-        if (err){
-          res.status(500).json({message: 'Something went wrong at loging in step. Please try again.'});
-          return;
-        }
-        theClient.password = undefined; // hides user's password from POST. Only shows in database.
-        res.status(200).json(theClient);
-      });
-    });
-  });
-});
 
 module.exports = router;
