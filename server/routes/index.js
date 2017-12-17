0c7f335f2d44fcdb7ec8263c2a900ce67127c5e6
Aaron Molina <aaronm-git@users.noreply.github.com>
2017-12-16 23:59:07 -0500

    fixed index js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,20 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+// var path = require('path');
 //
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+// module.exports = function(app) {
+//   app.use('/models/user-model', require('../models/user-model'));
 //
-// module.exports = router;
-
-var path = require('path');
-
-module.exports = function(app) {
-  app.use('/models/user-model', require('../models/user-model'));
-
-	// catch 404 and forward to Angular
-  app.all('/*', function (req, res) {
-    res.sendfile(__dirname + '/public/index.html');
-  });
-};
+// 	// catch 404 and forward to Angular
+//   app.all('/*', function (req, res) {
+//     res.sendfile(__dirname + '/public/index.html');
+//   });
+// };
