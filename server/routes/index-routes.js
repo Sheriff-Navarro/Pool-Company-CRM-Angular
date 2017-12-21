c23de7256ffbbfd7884971f62fc7ba66bfcc7e44
Aaron Molina <aaronm-git@users.noreply.github.com>
2017-12-20 23:48:38 -0500

    Added the index route code
    Angular will handle the view side of the homepage

---
+++ server/routes/index-routes.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next) => {
+}); // homepage
+
+// router.get('/about', (req, res, next) => {
+// });
+//
+// router.get('/contact', (req, res, next) => {
+// });
+
+module.exports = router;
