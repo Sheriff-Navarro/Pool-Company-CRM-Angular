b40bdd44fcb80dea680029f4552e35ab936499f0
Aaron Molina <aaronm-git@users.noreply.github.com>
2017-12-24 00:11:11 -0500

    protected the settings component from unauth users

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -25,6 +25,5 @@ export class DashboardComponent implements OnInit {
       .catch(() => {
           this.router.navigate(['/app/login']);
       });
-
   }
 }
--- client/src/app/settings/settings.component.ts
+++ client/src/app/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  currentUser: any = {};
+
+  constructor(
+    private AuthService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+
+    this.AuthService.checklogin()
+      .then((userFromApi) => {
+          this.currentUser = userFromApi;
+      })
+      .catch(() => {
+          this.router.navigate(['/app/login']);
+      });
   }
 
 }
