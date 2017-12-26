147bfa63242ae1b9cdde9ec67d1031c72a462ea0
Aaron Molina <aaronm-git@users.noreply.github.com>
2017-12-25 20:57:27 -0500

    navbar updated to read from database.
    username and profile pic readable and displayed

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,16 +10,26 @@ import { Router } from '@angular/router';
 })
 export class NavBarComponent implements OnInit {
 
+  currentUser = {
+    avatarUrl: '',
+    firstName: '',
+}
+
   constructor(
     private AuthService: AuthService,
-    private router: Router
+    private router: Router,
+    private UsersService: UsersService
   ) { }
 
   logoutError: string;
 
   ngOnInit() {
     this.AuthService.checklogin()
-
+    .then((userFromApi) => {
+        this.UsersService.getUser()
+        .subscribe(userFromApi => {
+      this.currentUser =  userFromApi  })
+    })
     .catch(() => {
       this.logoutError = 'Something went wrong. Please try to logout again.';
     });
