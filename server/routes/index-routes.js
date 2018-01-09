0cb896e1e695966518a8b30ca6a8205f06d20555
Aaron Molina <aaronm-git@users.noreply.github.com>
2018-01-09 16:00:03 -0500

    Code cleanup

--- server/routes/index-routes.js
+++ server/routes/index-routes.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => { // homepage
+router.get('/', (req, res, next) => {
+  a
 });
 
-// router.get('/about', (req, res, next) => {
-// });
-//
-// router.get('/contact', (req, res, next) => {
-// });
-
 module.exports = router;
+
+
+//GET ALL CLIENTS---------------------------------------------------------
+
+//GET ONE CLIENT----------------------------------------------------------
+
+//CREATE------------------------------------------------------------------
+
+//EDIT--------------------------------------------------------------------
+
+//DELETE------------------------------------------------------------------
