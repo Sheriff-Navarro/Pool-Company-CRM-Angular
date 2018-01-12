5653a89f47447ab2ba359553d42e170066f1d1a6
Aaron Molina <aaronm-git@users.noreply.github.com>
2018-01-12 12:50:12 -0500

    switching database to URI and built angular to server

--- server/config/database-setup.js
+++ server/config/database-setup.js
@@ -6,12 +6,12 @@ const dbName = 'finalprojectdb';
 
 
 // connect to the database
-mongoose.connect(`mongodb://localhost/${dbName}`);
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(`mongodb://localhost/${dbName}`);
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log(`Connected to the ${dbName} database`);
+  // console.log(`Connected to the ${dbName} database`);
 });
