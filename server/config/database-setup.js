7eec812e4f42cf47baef0912589ca52a7071d5f5
Aaron Molina <aaronm-git@users.noreply.github.com>
2018-01-12 13:32:36 -0500

    added .env so it runs both on heroku and local

--- server/config/database-setup.js
+++ server/config/database-setup.js
@@ -13,5 +13,5 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  // console.log(`Connected to the ${dbName} database`);
+  console.log(`Connected to the ${dbName} database`);
 });
